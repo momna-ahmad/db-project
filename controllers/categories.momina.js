4fd7e82a5bf99c28d39a42a0038e6ce6a2234ca5
momna-ahmad <momnaahmdd@gmail.com>
2024-12-22 15:40:31 +0500

    change

--- controllers/categories.momina.js
+++ controllers/categories.momina.js
@@ -6,7 +6,7 @@ let user = require("../models/user.model");
 let order = require("../models/order.model");
 let cart = require("../models/shoppingCart.model");
 
-
+/*
 
 router.get('/products/mensclothing/:page?', async (req,res)=>{
     try {
@@ -163,5 +163,41 @@ router.get('/products/mensclothing/:page?', async (req,res)=>{
     }
   
   })
+
+*/
+
+
+router.get('/products/:category/:page?', async (req,res)=>{
+  try {
+    let Category = req.params.category ;
+    // Find products where category is 'men'
+    let page = req.params.page;
+page = page ? Number(page) : 1;
+let pageSize = 6;
+let totalRecords = await Product.countDocuments();
+let totalPages = Math.ceil(totalRecords / pageSize);
+// return res.send({ page });
+let products = await Product.find({category : Category})
+  .limit(pageSize)
+  .skip((page - 1) * pageSize);
+    
+    // Check if products were found
+    if (products.length > 0) {
+      // Render a page to display the products or send the products as JSON
+      return res.render('./partials/productList', { products , layout : "basiclayout" , category : 'footwear',page,
+        pageSize,
+        totalPages,
+        totalRecords, } );
+    } else {
+      // If no products are found, return a message or render an empty product list
+      return res.render('./partials/noproductsfound',  { message: 'No products found in this category.' , layout : "basiclayout"});
+    }
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return res.status(500).send("An error occurred while fetching products.");
+  }
+
+})
+
 
 module.exports= router ;
